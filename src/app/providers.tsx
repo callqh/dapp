cd2de690115725762a73f514cb5b8407d3ad3759
callqh <528851597@qq.com>
2025-09-06 15:19:06 +0800

    fix: rainbowkit

---
+++ src/app/providers.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider } from 'wagmi';
+import { darkTheme, lightTheme, midnightTheme, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { config } from '../config/wagmi';
+import '@rainbow-me/rainbowkit/styles.css';
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider
+          showRecentTransactions
+          theme={
+          lightTheme({
+            ...lightTheme.accentColors.pink,
+              overlayBlur: "small"
+          })
+        }>
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
